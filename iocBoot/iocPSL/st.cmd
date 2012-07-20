4947852de18dd9cfe6f735c506b6cd84bbf8a7fc
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2012-07-20 23:33:02 +0000

    Numerous changes while developing
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@14893 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

--- iocBoot/iocPSL/st.cmd
+++ iocBoot/iocPSL/st.cmd
@@ -4,21 +4,34 @@ errlogInit(20000)
 dbLoadDatabase("$(AREA_DETECTOR)/dbd/PSLApp.dbd")
 PSLApp_registerRecordDeviceDriver(pdbbase) 
 
-epicsEnvSet("PREFIX", "13PSL1:")
+epicsEnvSet("PREFIX", "PSL1:")
 epicsEnvSet("PORT",   "PSL")
 epicsEnvSet("QSIZE",  "20")
-epicsEnvSet("XSIZE",  "2048")
-epicsEnvSet("YSIZE",  "2048")
+epicsEnvSet("XSIZE",  "4007")
+epicsEnvSet("YSIZE",  "2670")
 epicsEnvSet("NCHANS", "2048")
 
 ###
-# Create the asyn port to talk to the PSL server on port 5000
+# Create the asyn port to talk to the PSL server on port 50000
 # Set noAutoConnect and noProcessEos
-drvAsynIPPortConfigure("PSLServer","10.30.53.57:50000", 0, 1, 1)
-asynSetTraceMask("PSLServer",0,255)
-asynSetTraceIOMask("PSLServer",0,2)
+drvAsynIPPortConfigure("PSLServer","10.0.4.190:50000", 0, 0, 0)
+asynOctetSetOutputEos("PSLServer", 0, "\n")
 
-PSLConfig("$(PORT)", "PSLServer", 20, 200000000)
+asynSetTraceIOMask("PSLServer",0,2)
+#asynSetTraceMask("PSLServer",0,9)
+#asynSetTraceFile("PSLServer", 0, "PSLServer.out")
+
+
+# Need to open connection to camera - edit the camera name as needed
+# Note: subsequent running of the IOC will get an error that the camera is already open,
+# but that is OK
+asynOctetConnect PSLServer PSLServer 0 10
+asynOctetWriteRead PSLServer "Open;VHR11"
+
+PSLConfig("$(PORT)", "PSLServer", -1, -1)
+asynSetTraceIOMask("$(PORT)",0,2)
+#asynSetTraceMask("$(PORT)",0,9)
+
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/ADBase.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDFile.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/PSL.template",   "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
@@ -26,14 +39,12 @@ dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/PSL.template",   "P=$(PREFIX),R=cam1:,P
 # Create a standard arrays plugin
 NDStdArraysConfigure("Image1", 5, 0, "$(PORT)", 0, -1)
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),NDARRAY_ADDR=0")
-# Make NELEMENTS in the following be a little bigger than 2048*2048
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,TYPE=Int16,FTVL=SHORT,NELEMENTS=4200000")
+# Make NELEMENTS in the following be a little bigger than 4007*2670
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,TYPE=Int16,FTVL=SHORT,NELEMENTS=11000000")
 
 # Load all other plugins using commonPlugins.cmd
 < ../commonPlugins.cmd
 
-#asynSetTraceMask("$(PORT)",0,3)
-#asynSetTraceIOMask("$(PORT)",0,4)
 
 iocInit()
 
